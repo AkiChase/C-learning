3b48008166bb7a1325da8ed2b641864da5ed503c
如初 <1003019131@qq.com>
2021-08-31 12:05:22 +0800

    new

--- C语言基础第二章/C语言基础第二章/08if语句.c
+++ C语言基础第二章/C语言基础第二章/08if语句.c
@@ -1,7 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void func_08()
 {
+	printf("ÊäÈëscore£º");
 	int score;
-	scanf("%d")
+	scanf("%d", &score);
+	if (score > 700)
+	{
+		printf(">700");
+	}
+	else if (score > 600)
+	{
+		printf(">600");
+	}
+	else
+	{
+		printf("no");
+	}
+
 }
