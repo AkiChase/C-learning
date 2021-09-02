6385d66c802f5c808e1804a46cabd39fff8a6056
如初 <1003019131@qq.com>
2021-09-02 22:53:08 +0800

    new

---
+++ C语言基础第四章/C语言基础第四章/02随机数.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+void func_02()
+{
+
+}
---
+++ C语言基础第四章/C语言基础第四章/main.c
@@ -0,0 +1,6 @@
+
+int main(void)
+{
+	func_02();
+	return 0;
+}
